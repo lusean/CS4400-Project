de7e8f82b8f27e95998d12bf634d966c145353db
Ollie Foo <olliefoo@yahoo.com>
2016-11-28 17:36:47 -0500

    added registration functionality

--- phase3/src/controller/RegistrationController.java
+++ phase3/src/controller/RegistrationController.java
@@ -1,9 +1,13 @@
 package controller;
 
+import entity.Student;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class RegistrationController {
 
@@ -23,7 +27,29 @@ public class RegistrationController {
 
     @FXML
     private void handleCreatePressed() {
-        MainController.getInstance().changeScene("../view/LoginScreen.fxml", "Login");
+        if(isStudentValid()) {
+            Student student = new Student(usernameField.getText(), emailField.getText(), null, null);
+            try {
+                student.insert();
+            } catch (SQLException e) {
+                MainController.getInstance().showAlertMessage(e.getMessage());
+            }
+            MainController.getInstance().changeScene("../view/LoginScreen.fxml", "Login");
+        }
     }
 
+    private boolean isStudentValid() {
+        try {
+            List<Student> students = Student.selectAllStudents();
+            for(Student s : students) {
+                if(s.username.equals(usernameField.getText())) {
+                    return false;
+                }
+            }
+        } catch (SQLException e) {
+            MainController.getInstance().showAlertMessage(e.getMessage());
+        }
+
+        return true;
+    }
 }
