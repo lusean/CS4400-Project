a60790b431b8203bfa58b56633708d0d5ffd8aea
Devadas, Ananth R <ananthdevadas@gatech.edu>
2016-12-01 10:16:03 -0500

    Added cascade to delete ignoring fk

--- phase3/drop.sql
+++ phase3/drop.sql
@@ -1,1 +1,23 @@
-DROP TABLE IF EXISTS CourseCategories, Courses, ProjectCategories, StudentProjectApplications, Projects, ApplyStatuses, Designations, Categories, Students, Users, Majors, Departments, Years;
+DROP TABLE IF EXISTS CourseCategories CASCADE;
+
+DROP TABLE IF EXISTS Courses CASCADE;
+
+DROP TABLE IF EXISTS ProjectCategories CASCADE;
+
+DROP TABLE IF EXISTS StudentProjectApplications;
+
+DROP TABLE IF EXISTS Projects CASCADE;
+
+DROP TABLE IF EXISTS ApplyStatuses CASCADE;
+
+DROP TABLE IF EXISTS Designations CASCADE;
+
+DROP TABLE IF EXISTS Categories CASCADE;
+
+DROP TABLE IF EXISTS Students CASCADE;
+
+DROP TABLE IF EXISTS Users CASCADE;
+
+DROP TABLE IF EXISTS Majors CASCADE;
+
+DROP TABLE IF EXISTS Years CASCADE;
