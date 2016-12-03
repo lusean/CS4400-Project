e0c036f56fade9e93b709813f2a2b8fb63cec3fd
Sean Lu <slu81@gatech.edu>
2016-12-03 18:11:28 -0500

    Added project requirements in project detailview

--- phase3/src/controller/DetailsController.java
+++ phase3/src/controller/DetailsController.java
@@ -31,40 +31,35 @@ public class DetailsController {
             Course course = MainController.getInstance().getCourse();
             titleField.setText(course.courseNumber);
             // NEED TO DISPLAY COURSE CATEGORIES
-            String str = "";
+            String cat = "";
             try {
                 List<CourseCategory> list = CourseCategory.selectCourseCategoriesForCourse(course.courseNumber);
                 for (int i = 0; i < list.size(); i++) {
                     if (i != 0) {
-                        str += ", ";
+                        cat += ", ";
                     }
-                    str += list.get(i).category;
+                    cat += list.get(i).category;
                 }
             } catch (SQLException e){
                 MainController.getInstance().showAlertMessage(e.getMessage());
             }
             descriptionField.setText(String.format("Course Name: '%s'\n Instructor: '%s'\n Designation: '%s'\n" +
                     "Category: '%s'\n Estimated number of students: '%s'", course.courseName, course.instructor,
-                    course.designation, str, course.estimatedStudent));
+                    course.designation, cat, course.estimatedStudent));
         } else {
             Project project = MainController.getInstance().getProject();
             titleField.setText(project.projectName);
-            String str = "";
+            String cat = "";
+            String req = "";
             try {
-                List<ProjectCategory> list = ProjectCategory.selectCourseCategoriesForCourse(project.projectName);
-                for (int i = 0; i < list.size(); i++) {
-                    if (i != 0) {
-                        str += ", ";
-                    }
-                    str += list.get(i).category;
-                }
+                cat = getProjectCategories(project);
+                req = getProjectRequirements(project);
             } catch (SQLException e) {
                 MainController.getInstance().showAlertMessage(e.getMessage());
             }
-            String blank = "";
             descriptionField.setText(String.format("Advisor: '%s'\n Description: '%s'\n Designation: '%s'\n" +
                     "Category: '%s'\n Requirements: '%s'\n Estimated number of students '%s'", project.advisorName,
-                    project.description, project.designation, str, blank, project.estimatedStudents));
+                    project.description, project.designation, cat, req, project.estimatedStudents));
         }
     }
 
@@ -90,6 +85,42 @@ public class DetailsController {
         } else {
             MainController.getInstance().showAlertMessage("Please update your profile to include a year and major");
         }
+    }
+
+    private String getProjectRequirements(Project p) {
+        List<String> list = new ArrayList<>();
+        String str = "";
+        list.add(p.majorRestriction);
+        list.add(p.yearRestriction);
+        list.add(p.deptRestriction);
+        for (int i = 0; i < list.size(); i++) {
+            if (i != 0) {
+                str += ", ";
+            }
+            if (list.get(i) != null) {
+                str += list.get(i);
+            } else {
+                list.remove(i);
+                i--;
+            }
+        }
+        return str;
+    }
+
+    private String getProjectCategories(Project p) throws SQLException {
+        String str = "";
+        try {
+            List<ProjectCategory> list = ProjectCategory.selectProjectCategoriesForProject(p.projectName);
+            for (int i = 0; i < list.size(); i++) {
+                if (i != 0) {
+                    str += ", ";
+                }
+                str += list.get(i).category;
+            }
+        } catch (SQLException e) {
+            MainController.getInstance().showAlertMessage(e.getMessage());
+        }
+        return str;
     }
 }
 
